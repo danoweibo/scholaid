557b6cfb8b91dc0cbb5fa5b841f5baa8587dc39a
Daniel Oweibo <112868914+danoweibo@users.noreply.github.com>
2026-05-02 23:17:54 +0100

    Create fonts.ts

---
+++ lib/fonts.ts
@@ -0,0 +1,229 @@
+import localFont from "next/font/local";
+
+export const grotaFont = localFont({
+  src: [
+    {
+      path: "../public/fonts/grotasans-thin.woff2",
+      weight: "100",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/grotasans-thin-italic.woff2",
+      weight: "100",
+      style: "italic",
+    },
+    {
+      path: "../public/fonts/grotasans-light.woff2",
+      weight: "300",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/grotasans-light-italic.woff2",
+      weight: "300",
+      style: "italic",
+    },
+    {
+      path: "../public/fonts/grotasans-regular.woff2",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/grotasans-italic.woff2",
+      weight: "400",
+      style: "italic",
+    },
+    {
+      path: "../public/fonts/grotasans-medium.woff2",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/grotasans-medium-italic.woff2",
+      weight: "500",
+      style: "italic",
+    },
+    {
+      path: "../public/fonts/grotasans-semi-bold.woff2",
+      weight: "600",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/grotasans-semi-bold-italic.woff2",
+      weight: "600",
+      style: "italic",
+    },
+    {
+      path: "../public/fonts/grotasans-bold.woff2",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/grotasans-bold-italic.woff2",
+      weight: "700",
+      style: "italic",
+    },
+    {
+      path: "../public/fonts/grotasans-extra-bold.woff2",
+      weight: "800",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/grotasans-extra-bold-italic.woff2",
+      weight: "800",
+      style: "italic",
+    },
+    {
+      path: "../public/fonts/grotasans-black.woff2",
+      weight: "900",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/grotasans-black-italic.woff2",
+      weight: "900",
+      style: "italic",
+    },
+    {
+      path: "../public/fonts/grotasans-heavy.woff2",
+      weight: "950",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/grotasans-heavy-italic.woff2",
+      weight: "950",
+      style: "italic",
+    },
+  ],
+  variable: "--font-grota",
+  display: "swap",
+});
+
+export const jakartaFont = localFont({
+  src: [
+    {
+      path: "../public/fonts/plusjakartasans-extralight.woff2",
+      weight: "200",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/plusjakartasans-extralight-italic.woff2",
+      weight: "200",
+      style: "italic",
+    },
+    {
+      path: "../public/fonts/plusjakartasans-light.woff2",
+      weight: "300",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/plusjakartasans-light-italic.woff2",
+      weight: "300",
+      style: "italic",
+    },
+    {
+      path: "../public/fonts/plusjakartasans-regular.woff2",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/plusjakartasans-italic.woff2",
+      weight: "400",
+      style: "italic",
+    },
+    {
+      path: "../public/fonts/plusjakartasans-medium.woff2",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/plusjakartasans-medium-italic.woff2",
+      weight: "500",
+      style: "italic",
+    },
+    {
+      path: "../public/fonts/plusjakartasans-semibold.woff2",
+      weight: "600",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/plusjakartasans-semibold-italic.woff2",
+      weight: "600",
+      style: "italic",
+    },
+    {
+      path: "../public/fonts/plusjakartasans-bold.woff2",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/plusjakartasans-bold-italic.woff2",
+      weight: "700",
+      style: "italic",
+    },
+    {
+      path: "../public/fonts/plusjakartasans-extrabold.woff2",
+      weight: "800",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/plusjakartasans-extrabold-italic.woff2",
+      weight: "800",
+      style: "italic",
+    },
+  ],
+  variable: "--font-jakarta",
+  display: "swap",
+});
+
+export const tiktokFont = localFont({
+  src: [
+    {
+      path: "../public/fonts/tiktoksans-light.woff2",
+      weight: "300",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/tiktoksans-regular.woff2",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/tiktoksans-medium.woff2",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/tiktoksans-semibold.woff2",
+      weight: "600",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/tiktoksans-bold.woff2",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/tiktoksans-extrabold.woff2",
+      weight: "800",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/tiktoksans-black.woff2",
+      weight: "900",
+      style: "normal",
+    },
+  ],
+  variable: "--font-tiktok",
+  display: "swap",
+});
+
+export const momoFont = localFont({
+  src: [
+    {
+      path: "../public/fonts/momosignature-regular.woff2",
+      weight: "400",
+      style: "normal",
+    },
+  ],
+  variable: "--font-momo",
+  display: "swap",
+});
