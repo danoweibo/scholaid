9d4aa15c39137007b2c8bf2d19eabd6cbde5313f
Daniel Oweibo <112868914+danoweibo@users.noreply.github.com>
2026-05-03 21:25:47 +0100

    update deps

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
-import { Card } from "@repo/ui/card";
-import { Gradient } from "@repo/ui/gradient";
-import { TurborepoLogo } from "@repo/ui/turborepo-logo";
+import { Card } from "@scholaid/ui/card";
+import { Gradient } from "@scholaid/ui/gradient";
+import { TurborepoLogo } from "@scholaid/ui/turborepo-logo";
 
 const LINKS = [
   {
