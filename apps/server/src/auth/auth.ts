161ecb913243677c61c13287056ba9c41745a4ff
Daniel Oweibo <112868914+danoweibo@users.noreply.github.com>
2026-05-05 10:18:49 +0100

    update: better-auth setup [001]

---
+++ apps/server/src/auth/auth.ts
@@ -0,0 +1,30 @@
+import { db } from '@/db';
+import { betterAuth } from 'better-auth';
+import { drizzleAdapter } from 'better-auth/adapters/drizzle';
+
+export const auth = betterAuth({
+  baseURL: process.env.BETTER_AUTH_URL,
+  database: drizzleAdapter(db, {
+    provider: 'pg',
+  }),
+  emailAndPassword: {
+    enabled: true,
+  },
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    },
+    linkedin: {
+      clientId: process.env.LINKEDIN_CLIENT_ID as string,
+      clientSecret: process.env.LINKEDIN_CLIENT_SECRET as string,
+    },
+    microsoft: {
+      clientId: process.env.MICROSOFT_CLIENT_ID as string,
+      clientSecret: process.env.MICROSOFT_CLIENT_SECRET as string,
+      tenantId: 'common',
+      authority: 'https://login.microsoftonline.com', // Authentication authority URL
+      prompt: 'select_account', // Forces account selection
+    },
+  },
+});
---
+++ apps/server/src/main.ts
@@ -0,0 +1,19 @@
+/* eslint-disable @typescript-eslint/no-floating-promises */
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+    {
+      bodyParser: false,
+    },
+  );
+  await app.listen(5000, '0.0.0.0');
+}
+bootstrap();
