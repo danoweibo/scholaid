5e8181c0f40746dc1160cefa866673f37052f470
Daniel Oweibo <112868914+danoweibo@users.noreply.github.com>
2026-05-06 16:26:34 +0100

    update: lenis scroll + app config

--- apps/web/providers/lenis.tsx
+++ apps/web/providers/lenis.tsx
@@ -1,17 +1,17 @@
-"use client"
+"use client";
 
-import { useEffect } from "react"
-import Lenis from "lenis"
+import { useEffect } from "react";
+import Lenis from "lenis";
 
 export function LenisProvider({ children }: { children: React.ReactNode }) {
   useEffect(() => {
-    const lenis = new Lenis()
+    const lenis = new Lenis();
     function raf(time: number) {
-      lenis.raf(time)
-      requestAnimationFrame(raf)
+      lenis.raf(time);
+      requestAnimationFrame(raf);
     }
-    requestAnimationFrame(raf)
-    return () => lenis.destroy()
-  }, [])
-  return <>{children}</>
+    requestAnimationFrame(raf);
+    return () => lenis.destroy();
+  }, []);
+  return <>{children}</>;
 }
