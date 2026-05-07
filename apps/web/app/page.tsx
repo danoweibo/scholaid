7909aa925a88b669b76529d572f83888f6282e63
Daniel Oweibo <112868914+danoweibo@users.noreply.github.com>
2026-05-07 17:08:05 +0100

    feat: hero ui on client

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,9 +1,12 @@
+import HeroSection from "@/blocks/lander/hero-section";
+
 export default function Page() {
   return (
     <main>
-      <h1 className="font-apfel mt-10 text-center text-4xl font-bold">
+      <h1 className="font-apfel-mittel mt-10 text-center text-4xl font-bold">
         Welcome to Scholaid!
       </h1>
+      <HeroSection />
     </main>
   );
 }
