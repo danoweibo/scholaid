8620d02a34d32318c0934307e5846aca493c7165
Daniel Oweibo <112868914+danoweibo@users.noreply.github.com>
2026-05-14 23:23:04 +0100

    update page.tsx

--- apps/web/app/signin/page.tsx
+++ apps/web/app/signin/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { LoginForm } from "@/components/login-form";
+import { LoginForm } from "@/components/shadcn/login-form";
 import Image from "next/image";
 import { useState } from "react";
 
