b80d330dbd722d1d1aafb55b67b08f1ae531b679
Daniel Oweibo <112868914+danoweibo@users.noreply.github.com>
2026-05-19 11:00:59 +0100

    rollback next config

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -2,14 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   devIndicators: false,
-  async rewrites() {
-    return [
-      {
-        source: "/api/:path*",
-        destination: "https://backboard.scholaid.co/api/:path*",
-      },
-    ];
-  },
   typescript: {
     ignoreBuildErrors: true,
   },
