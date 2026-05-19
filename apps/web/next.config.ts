8c8af4535b7ef499f7dabe38ffe4f9d77d886105
Daniel Oweibo <112868914+danoweibo@users.noreply.github.com>
2026-05-19 07:25:06 +0100

    fix: better auth url

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -6,7 +6,7 @@ const nextConfig: NextConfig = {
     return [
       {
         source: "/api/:path*",
-        destination: `${process.env.NEXT_PUBLIC_SERVER_URL}/api/:path*`,
+        destination: `${process.env.SERVER_URL}/api/:path*`,
       },
     ];
   },
