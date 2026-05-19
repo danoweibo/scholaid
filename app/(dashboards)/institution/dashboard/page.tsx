413f1e644a1e35ebab5fe0b8e1a014ffc5ecd67d
Daniel Oweibo <112868914+danoweibo@users.noreply.github.com>
2026-05-19 15:10:52 +0100

    fix: export react comp

---
+++ app/(dashboards)/institution/dashboard/page.tsx
@@ -0,0 +1,8 @@
+export default function DashboardPage() {
+  return (
+    <div>
+      <h1>Institution Dashboard</h1>
+      {/* Add your dashboard components and content here */}
+    </div>
+  );
+}
---
+++ app/(dashboards)/student/dashboard/page.tsx
@@ -0,0 +1,8 @@
+export default function StudentPage() {
+  return (
+    <div>
+      <h1>Student Dashboard</h1>
+      {/* Add your dashboard components and content here */}
+    </div>
+  );
+}
