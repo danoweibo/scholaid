7f1c8e9f40c69bab8b29d4885eb49d22cb08c0d2
Daniel Oweibo <112868914+danoweibo@users.noreply.github.com>
2026-05-23 23:36:25 +0100

    eject redirect scripts(index)

--- app/(dashboards)/institution/page.tsx
+++
@@ -1,5 +0,0 @@
-import { redirect } from "next/navigation";
-
-export default function InstitutionRedirect() {
-  redirect("/institution/dashboard");
-}
--- app/(dashboards)/student/page.tsx
+++
@@ -1,5 +0,0 @@
-import { redirect } from "next/navigation";
-
-export default function StudentRedirect() {
-  redirect("/student/dashboard");
-}
