549faf776252a78e323172723b4a055242bb63ea
Daniel Oweibo <112868914+danoweibo@users.noreply.github.com>
2026-07-02 23:29:52 +0100

    update: user `auth` routes

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -14,7 +14,7 @@ export default function LoginPage() {
           <a href="#" className="flex items-center gap-2 font-medium">
             <Image
               src="/images/logotype-1.png"
-              alt="Scholaid"
+              alt="scholaid"
               width={120}
               height={30}
               className="h-auto w-30 object-contain"
